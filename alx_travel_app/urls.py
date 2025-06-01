19db5cf32fe98b817c2b166ba3c30c57d3e12e9f
unknown <edwineduuh770@gmail.com>
2025-06-01 15:05:39 +0300

    Add correct API endpoints

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -20,7 +20,7 @@ Including another URLconf
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from django.contrib import admin
@@ -38,6 +38,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
