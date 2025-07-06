d9bae607cb4229a3a73988a33d00a1c025fcd826
unknown <edwineduuh770@gmail.com>
2025-07-06 21:55:13 +0300

    Modify listings

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import ListingSerializer
+
 
 class HelloView(APIView):
     def get(self, request):
